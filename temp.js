48b43d3e0a39ab1694a5e81fc228254e6f752470
2018SmartRoom <38051713+2018SmartRoom@users.noreply.github.com>
2018-04-22 02:21:38 -0400

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,25 @@
+//Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCPAlQ0rX2XkBuc62zKuOqvLOwXLy3n_fo",
+    authDomain: "separatetempcontroltest.firebaseapp.com",
+    databaseURL: "https://separatetempcontroltest.firebaseio.com",
+    projectId: "separatetempcontroltest",
+    storageBucket: "separatetempcontroltest.appspot.com",
+    messagingSenderId: "177653835432"
+  };
+  firebase.initializeApp(config);
+// create object
+const preObject1 = document.getElementById('humidity');
+const preObject = document.getElementById('temperature');
+
+//create references
+const dbRefObject1 = firebase.database().ref().child('humidity');
+const dbRefObject = firebase.database().ref().child('temperature');
+
+//sync changes
+dbRefObject1.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(), null, 3);
+});
+dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(), null, 3);
+});
