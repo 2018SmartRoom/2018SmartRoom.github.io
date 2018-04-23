7b779b3e3db5c9089cc69408dddbdd38854ca4c1
2018SmartRoom <38051713+2018SmartRoom@users.noreply.github.com>
2018-04-23 19:00:04 -0400

    Update temp.js

--- temp.js
+++ temp.js
@@ -8,24 +8,18 @@
     messagingSenderId: "177653835432"
   };
   firebase.initializeApp(config);
-
-var ref = firebase.database().ref();
-
-ref.on("value", function(snapshot){
-    output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
-});
 // create object
-//const preObject = document.getElementById('humidity');
-//const preObject1 = document.getElementById('temperature');
+const preObject = document.getElementById('humidity');
+const preObject1 = document.getElementById('temperature');
 
 //create references
-//const dbRefObject = firebase.database().ref().child('humidity');
-//const dbRefObject1 = firebase.database().ref().child('temperature');
+const dbRefObject = firebase.database().ref().child('humidity');
+const dbRefObject1 = firebase.database().ref().child('temperature');
 
-//sync changes
-//dbRefObject.on('value', snap => {
-//    preObject.innerText = JSON.stringify(snap.val(), null, 3);
-//});
-//dbRefObject1.on('value', snap => {
-//    preObject1.innerText = JSON.stringify(snap.val(), null, 3);
-//});
+//sync changess
+dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(), null, 3);
+});
+dbRefObject1.on('value', snap => {
+    preObject1.innerText = JSON.stringify(snap.val(), null, 3);
+});
