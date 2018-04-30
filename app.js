b9846b41b9ad8643237abff1c13ba4fc9a2e67b0
2018SmartRoom <38051713+2018SmartRoom@users.noreply.github.com>
2018-04-30 01:20:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,30 +8,7 @@
     messagingSenderId: "119109846151"
   };
   firebase.initializeApp(config);
-  database = firebase.database();
-
-var ref = database.ref('distance');
-ref.on('value', gotData, errData);   
-
-function gotData(data) { 
- // console.log(data.val());
-  var distance = data.val();
-  // Grab the keys to iterate over the object
-  var keys = Object.keys(distance);
-  console.log(keys);
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    // Look at each distance object!
-    var distances = distance[key].distances;
-    console.log(distances);
-  }
-}
-
-function errData(err) {
- console.log('Error');
- console.log(err);
-}
-/* 
+  
 // get elements
 const preObject = document.getElementById('SECURITY');
 const ulList = document.getElementById('list');
@@ -48,4 +25,3 @@ dbRefObject.on('value', snap => {
 //list changes
 dbRefList.on('child_added', snap => console.log(snap.val()));
 
-*/
