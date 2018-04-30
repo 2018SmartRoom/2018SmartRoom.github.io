d5faa28a439ab50230dae0691d7e5b2eaae91ffe
2018SmartRoom <38051713+2018SmartRoom@users.noreply.github.com>
2018-04-30 00:31:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,22 @@
   };
   firebase.initializeApp(config);
 
+var ref = database.ref("distance);
+ref.on("value", gotData, errData);
+
+function gotData(data) {
+  var distance = data.val();
+  // Grab the keys to iterate over the object
+  var keys = Object.keys(distance);
+
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    // Look at each fruit object!
+    var distance = distance[key];
+  }
+}
+
+/* 
 // get elements
 const preObject = document.getElementById('SECURITY');
 const ulList = document.getElementById('list');
@@ -25,3 +41,4 @@ dbRefObject.on('value', snap => {
 //list changes
 dbRefList.on('child_added', snap => console.log(snap.val()));
 
+*/
